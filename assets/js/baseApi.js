768ef0127d9a8c5eee013040582826e6e54a3aa1
itxy <itxy@xydm.cn>
2023-04-17 11:17:16 +0800

    完成主页的功能开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,14 +1,37 @@
 /**ajaxPrefilter()解析
- * 1. 注意： 每次调用jQuery里面的 $.get() 或 $.post() 或 $.ajax() 的时候， 
+ * 1. 注意： 每次调用jQuery里面的 $.get() 或 $.post() 或 $.ajax() 的时候，
  * 2. 都会先执行调用 ajaxPrefilter() 这个函数
  * 3. 这个函数执行的时候， 可以获取到我们给Ajax提供的配置对象(options)
- * 4. 所以只有这个函数执行执行完毕后，才会发起真正的请求, 
+ * 4. 所以只有这个函数执行执行完毕后，才会发起真正的请求,
  * 5. 所以在利用这个函数的特性， 在发器真正的请求的时候，我们可以拼接URL等等
  */
 
-$.ajaxPrefilter(function(options) {
-    console.log(options.url);
-    // 在发起真正的 Ajax 请求之前，进行统一拼接请求的根路径
-    options.url = "http://www.liulongbin.top:3007" + options.url;
-    console.log(options.url);
-})
+$.ajaxPrefilter(function (options) {
+  // 在发起真正的 Ajax 请求之前，进行统一拼接请求的根路径
+  options.url = "http://www.liulongbin.top:3007" + options.url;
+
+  // 统一为有权限的接口，设置 headers 请求头
+  // 通过indexOf 查找有没有 /my/这个字符串，
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+
+  // 全局统一挂载 complete 回调函数  控制用户的访问权限
+ //   complete: 不论成功还是失败， 最终都会调用 complete 回调函数
+  options.complete = function (res) {
+    // console.log('执行了complete回调函数');
+    // console.log(res);
+    // complete 回调函数中 可以使用 responseJSON 拿到服务响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      // 1. 强制清空 token
+      localStorage.removeItem("token");
+      //2. 强制跳转到登录页面
+      location.href = "/login.html";
+    }
+  };
+});
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+$(function() {
+    // 1. 获取用户的基本信息
+    getUserInfo();
+    let layer = layui.layer;
+    // 3. 点击退出，退出到登录界面上
+    $('#btnIndexOut').on('click', function() {
+        // 利用layui的内置模块中confirm 询问函数，来判断用户是否确认退出
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            // 当点击确认退出后，此时要做两件事
+            // 1. 清空本地存储的 token
+            localStorage.removeItem('token');
+            // 2. 重新跳转到登录界面
+            location.href = '/login.html';
+            // 3. 这句代码一定要带上，这是layui官方提供的
+            layer.close(index);
+         });
+    })
+})
+
+//1. 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        // 这个请求接口，是一个有权限的接口。所以要在headers 上添加一些验证字段
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status != 0) {
+                return layui.layer.msg('获取用户信息失败');
+            }
+            renderAvater(res.data);
+        },
+    })
+}
+
+//2. 渲染用户的头像
+function renderAvater(dataObj) {
+    let uname = dataObj.nickname || dataObj.username;
+    $('#welcome').html('欢迎:' + uname);
+    if (dataObj.user_pic !== null) {
+        $('.layui-nav-img').attr('src', dataObj.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+       $('.layui-nav-img').hide();
+       $('.text-avatar').html(uname[5].toUpperCase()).show();
+    }
+
+}
